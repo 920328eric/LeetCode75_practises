87c387cff6748789bd0592c7cb63d20bc314e022
陳重宇 <12345eric920328@gmail.com>
2024-07-08 22:56:02 +0800

    7/8 solve 6 with O(n) extra space

---
+++ 6_O(1)_extra spaceReverse Words in a String.cpp
@@ -0,0 +1,5 @@
+// topic：151. Reverse Words in a String
+// runtime： ms
+// space : MB 
+// O(1) extra space
+
--- 6_O(n)_extra spaceReverse Words in a String.cpp
+++ 6_O(n)_extra spaceReverse Words in a String.cpp
@@ -1,31 +1,39 @@
 // topic：151. Reverse Words in a String
-// runtime： ms
-// space : MB
+// runtime：3 ms
+// space :10.10 MB 
+// O(n) extra space
+
+// vector 會自己擴張容量大小的陣列。所以可以隨便加入或移除元素。
+// output.push_back(temp);
 
 class Solution {
 public:
     string reverseWords(string s) {
         int sLen = s.size();
-        vector<string> output(sLen);
+        vector<string> output;
         string temp;
         string outoutput;
-        int j = 0;
         for(int i = 0;i<sLen;i++){
             if(s[i] != ' '){
                 temp+=s[i];
             }
             else{
-                output[j] = temp;
-                temp = "";
-                j++;
+                // 遇到字的時候，前面temp為空字串，就變空格加到output裡
+                if(!temp.empty()){
+                    output.push_back(temp);
+                    temp.clear();
+                }
             }
+        }
+        if(!temp.empty()){
+            output.push_back(temp);
         }
         reverse(output.begin(),output.end());
         int outputLen = output.size(); 
         for(int i = 0;i<outputLen;i++){
             outoutput += output[i];
             if(i != (outputLen -1)){
-                outoutput += " ";
+                outoutput += ' ';
             }   
         }
         return outoutput;
