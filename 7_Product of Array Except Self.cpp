8519c72b7b1f78065d45626f99874a5b9badad15
陳重宇 <12345eric920328@gmail.com>
2024-07-10 22:51:26 +0800

    7/10 solve 7 with O(1) extra space and O(n) time

---
+++ 7_Product of Array Except Self.cpp
@@ -0,0 +1,24 @@
+// 238. Product of Array Except Self
+// runtime：20 ms
+// space :38.06 MB 
+// O(1) extra space and O(n) time
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int numsLen = nums.size();
+        vector<int> output(numsLen,1);
+        int sum = 0;
+        int left = 1;
+        for(int j = 0;j<numsLen;j++){
+            output[j] = left;
+            left *= nums[j];
+        }
+        int right = 1;
+        for(int i = (numsLen-1);i>=0;i--){
+            output[i] *= right;
+            right *= nums[i];
+        }
+        return output;
+    }
+};
