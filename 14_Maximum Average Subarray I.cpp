6358a834428ff87f06388c02c824beb56f078ea2
陳重宇 <12345eric920328@gmail.com>
2024-07-17 22:48:32 +0800

    7/17 change 14 the way of writing

--- 14_Maximum Average Subarray I.cpp
+++ 14_Maximum Average Subarray I.cpp
@@ -6,16 +6,16 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        double maxnum = 0.0;
-        double sum = 0.0;
+        double tempsum = 0.0;
+        double maxsum;
         for(int i = 0;i<k;i++){
-            sum+=nums[i]; // sum of the nums which start from 0 to k-1
+            tempsum+=nums[i]; // sum of the nums which start from 0 to k-1
         }
-        maxnum = sum;
-        for(int j=k;j<nums.size();j++){ 
-            sum = sum - nums[j-k] + nums[j]; // then cut of the nums[tail] and add nums[header+1],continue to folow this idea
-            maxnum = max(maxnum,sum);
+        maxsum = tempsum;
+        for(int j = k;j<nums.size();j++){
+            tempsum = tempsum - nums[j-k] + nums[j]; // then cut of the nums[tail] and add nums[header+1],continue to folow this idea
+            maxsum = max(maxsum,tempsum);
         }
-        return maxnum/k;
+        return maxsum/k;
     }
 };
