2384823117f41404e970ac46fb8c95c6f677a971
陳重宇 <12345eric920328@gmail.com>
2024-08-25 23:36:24 +0800

    8/25 modify 44

--- 44_Number of Provinces.cpp
+++ 44_Number of Provinces.cpp
@@ -1,24 +1,24 @@
 // 547. Number of Provinces
-// runtime：21 ms Beats 58.92%
-// space : 18.76 MB Beats 59.67%
+// runtime：18 ms Beats 80.03%
+// space : 18.71 MB Beats 59.87%
 
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        vector<bool> vis(n,false); // 判斷 node 之間有無聯通，已有和別人聯通就為 true
+        vector<bool> con(n,false); // 判斷 node 之間有無聯通，已有和別人聯通就為 true
         int ans=0;
         for(int i=0;i<n;i++){
-            if(vis[i]==false){ // 去發現有沒有沒和別人聯通的 node
-                vis[i]=true;
+            if(con[i]==false){ // 去發現有沒有沒和別人聯通的 node
+                con[i]=true; 
                 stack<int> s;
                 s.push(i);
                 while(!s.empty()){ // 進行 DFS
                     int temp=s.top();
                     s.pop();
-                    for(int j=0;j<n;j++){ 
-                        if(isConnected[temp][j]==1&&vis[j]==false){ // 去看 node 裡，要和誰聯通，可連成的把他設 true
-                            vis[j]=true;
+                    for(int j=0;j<n;j++){
+                        if(isConnected[temp][j]==1&&con[j]==false){ // 去看 node 裡，要和誰聯通，可連成的把他設 true
+                            con[j]=true;
                             s.push(j);
                         }
                     }
