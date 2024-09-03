024fc02779fe5d7e17adc947ad4d7594b42d9d79
陳重宇 <12345eric920328@gmail.com>
2024-09-03 23:40:12 +0800

    9/3 solve 56 and modify 54

--- 54_Successful Pairs of Spells and Potions.cpp
+++ 54_Successful Pairs of Spells and Potions.cpp
@@ -1,6 +1,6 @@
 // 2300. Successful Pairs of Spells and Potions
-// runtime : 160 ms Beats 20.41%
-// space : 100.90 MB Beats 87.84%
+// runtime : 145 ms Beats 71.67%
+// space : 100.99 MB Beats 83.36%
 
 // # Approach
 // 找到一組乘績的右半部都是 > success，所以必須先由小到大排序 potions
@@ -8,26 +8,26 @@
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        int ns=spells.size();
+        int ss=spells.size();
         int ps=potions.size();
-        vector<int> ans(ns,0); // 先全部設為 0，因為如果沒有任何一組乘績 > success 就為 0
+        vector<int> ans(ss,0); // 先全部設為 0，因為如果沒有任何一組乘績 > success 就為 0
         sort(potions.begin(),potions.end());
-        for(int i=0;i<ns;i++){
-            int start=0,end=ps-1;
+        for(int i=0;i<ss;i++){
+            int first=0,end=ps-1;
             int current=-1; // 沒有任何一組乘績 > success，current 值就設 -1
-            while(start<=end){
-                int mid=start+(end-start)/2; // 避免溢位
-                long long num=spells[i]; // 避免乘績溢位
-                if(num*potions[mid]>=success){ // 找到一組乘績的右半部都是 > success
+            while(first<=end){
+                int mid=first+(end-first)/2; // 避免溢位
+                long long s=spells[i]; // 避免乘績溢位
+                if(s*potions[mid]>=success){ // 找到一組乘績的右半部都是 > success
                     end=mid-1;
-                    current=mid; 
+                    current=mid;
                 }
                 else{
-                    start=mid+1;
+                    first=mid+1;
                 }
-            }
-            if(current!=-1){ // 如果 current 值有改變，代表有找到一組乘績的右半部都是 > success
-                ans[i]=ps-current;
+                if(current!=-1){ // 如果 current 值有改變，代表有找到一組乘績的右半部都是 > success
+                    ans[i]=ps-current;
+                }
             }
         }
         return ans;
--- 56_Koko Eating Bananas.cpp
+++ 56_Koko Eating Bananas.cpp
@@ -1,6 +1,31 @@
 // 875. Koko Eating Bananas
-// runtime : 
-// space : 
+// runtime : 28 ms Beats 76.92%
+// space : 22.53 MB Beats 10.50%
 
-// 除法取上限公式：(x + y - 1) / y
+// # Approach
+//     1.由 1 到 最大堆香蕉數，一個一個去算加起來的時間跟 h 做比較，看有無超過時間
+//     2.除法取上限公式：(x + y - 1) / y
 
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        sort(piles.begin(),piles.end());
+        int start=1; // 最小有可能一次吃一根
+        int end=piles[n-1];
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            long long sum=0; // 避免溢位
+            for(int i=0;i<n;i++){
+                sum+=(piles[i]+mid-1)/mid; // 一個一個去除並取上限，加起來的時間，跟 h 做比較
+            }
+            if(sum>h){ // 花太多時間，所以要吃快一點，所以往右找
+                start=mid+1;
+            }
+            else{ // 可能花剛好時間，或花太少時間，所以可以吃慢一點，所以往左找
+                end=mid-1;
+            }
+        }
+        return start; // 舉個例子就會知道要輸出 start，還有 while 結束的條件也是 start > end
+    }
+};
